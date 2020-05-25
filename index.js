d98d7abe92d52a0b7f169ba19bac580f41aacfb5
Adam Klein <adam@500tech.com>
2020-05-25 09:24:30 +0300

    Adding GIT commands

--- index.js
+++ index.js
@@ -1,8 +1,45 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
+const { exec } = require("child_process");
+
+function execCommand(name, command) {
+  return new Promise((resolve, reject) => {
+    exec(command, (error, stdout, stderr) => {
+      if (error) {
+        core.setFailed(error);
+        console.log(`${name} error: ${error.message}`);
+        reject(error);
+
+        return;
+      }
+      if (stderr) {
+        core.setFailed(stderr);
+        console.log(`${name} stderr: ${stderr}`);
+        reject(stderr);
+
+        return;
+      }
+      console.log(`${name}: ${stdout}`);
+      resolve(stdout);
+    });
+  });
+}
+
+async function gitMergeCheck(branch) {
+  const command = `git merge-tree $(git merge-base HEAD ${branch}) HEAD ${branch} | grep -q '^+=======$' && echo conflict || echo "no conflict"`;
+  const result = await execCommand("listBranches", command);
+}
+
+async function listBranches() {
+  const command = "git branch -aq";
+  const result = await execCommand("listBranches", command);
+}
 
 try {
   // `who-to-greet` input defined in action metadata file
+  listBranches();
+  gitMergeCheck("conflicted_branch");
+  gitMergeCheck("nonconflicted_branch");
   const nameToGreet = core.getInput("who-to-greet");
   console.log(`Hello ${nameToGreet}!`);
   const time = new Date().toTimeString();
