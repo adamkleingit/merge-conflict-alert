a71978cbea5a47c885d8b8b462e080345677b87a
adam klein <adamklein.mail@gmail.com>
2020-05-25 14:14:11 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,13 +4,24 @@ const exec = require("@actions/exec");
 const { exec: childProcessExec } = require("child_process");
 
 let pwd;
-async function execCommand(name, command, args = []) {
+async function execCommand(command, args = []) {
   const options = pwd ? { cwd: `${pwd}/master` } : {};
   console.log('options', options);
   console.log('command', command);
   console.log('args', args);
-  return exec.exec(command, args, options);
-//   return new Promise((resolve, reject) => {
+  return new Promise((resolve, reject) => {
+    options.listeners = {
+      stdout: (data) => {
+        const stdout = data.toString();
+        resolve(stdout);
+      },
+      stderr: (data) => {
+        const stderr = data.toString();
+        resolve(stderr);
+      }
+    };
+
+    exec.exec(command, args, options);
 //     childProcessExec(`${command} ${args.join(' ')}`, options, (error, stdout, stderr) => {
 //       if (error) {
 //         console.error(`exec error: ${error}`);
@@ -22,32 +33,35 @@ async function execCommand(name, command, args = []) {
 //       console.error(`stderr: ${stderr}`);
 //       resolve(stdout);
 //     });
-//   });
+  });
 }
 
 async function gitMergeCheck(branch) {
-  const command = `git merge-tree $(git merge-base HEAD ${branch}) HEAD ${branch} | grep -q '^+=======$' && echo conflict || echo "no conflict"`;
-  return execCommand("listBranches", command);
+  const mergeBase = await execCommand('git', ['merge-base', 'HEAD', branch]);
+  return execCommand('git', ['merge-tree', mergeBase, 'HEAD', branch]);
+  // lookup for ^+=======$
 }
 
 async function listBranches() {
-  return execCommand("listBranches", "git", ["branch", "-q"]);
+  return execCommand("git", ["branch", "-q"]);
 }
 
 async function run() {
   try {
     // `who-to-greet` input defined in action metadata file
-    pwd = await exec.exec('pwd');
+    pwd = await execCommand('pwd');
     console.log('pwd', pwd);
-    const ls = await exec.exec('ls', [`${pwd}/master`]);
+    const ls = await execCommand('ls', [`${pwd}/master`]);
     console.log('ls', ls);
-    const branches = await exec.exec('git', ['branch'], {cwd: `${pwd}/master`});
+    const branches = await listBranches();
     console.log('branches', branches);
 //     const branches = await listBranches();
 //     console.log("branches", branches);
     // await gitMergeCheck("conflicted_branch");
     // await gitMergeCheck("nonconflicted_branch");
-    const nameToGreet = core.getInput("who-to-greet");
+    const 
+    
+    ToGreet = core.getInput("who-to-greet");
     console.log(`Hello ${nameToGreet}!`);
     const time = new Date().toTimeString();
     core.setOutput("time", time);
