2051f478369f241c4f75eb5f3585b845bc18afdc
adam klein <adam@500tech.com>
2020-05-25 12:32:49 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,10 +3,11 @@ const github = require("@actions/github");
 const exec = require("@actions/exec");
 const { exec: childProcessExec } = require("child_process");
 
-async function execCommand(name, command) {
-  return exec.exec(command);
+async function execCommand(name, command, args) {
+  const options = { cwd: "./master" };
+  return exec.exec(command, args, options);
   return new Promise((resolve, reject) => {
-    childProcessExec(command, { cwd: "./master" }, (error, stdout, stderr) => {
+    childProcessExec(`command ${args.join(' ')}`, options, (error, stdout, stderr) => {
       if (error) {
         console.error(`exec error: ${error}`);
         core.setFailed(error);
@@ -22,11 +23,11 @@ async function execCommand(name, command) {
 
 async function gitMergeCheck(branch) {
   const command = `git merge-tree $(git merge-base HEAD ${branch}) HEAD ${branch} | grep -q '^+=======$' && echo conflict || echo "no conflict"`;
-  return execCommand("listBranches", command);
+  return execCommand("listBranches", command, []);
 }
 
 async function listBranches() {
-  return execCommand("listBranches", "git branch -q");
+  return execCommand("listBranches", "git", ["branch", "-q"]);
 }
 
 async function run() {
