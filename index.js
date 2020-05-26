25e675a70db4c0a8ed895c9a3b153f975a9594af
adam klein <adamklein.mail@gmail.com>
2020-05-26 13:15:34 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
 const exec = require("@actions/exec");
-const { exec: childProcessExec } = require("child_process");
 
 function execCommand(command, args = []) {
   const options = { cwd: process.env.GITHUB_WORKSPACE };
@@ -9,59 +8,46 @@ function execCommand(command, args = []) {
   console.log('command', command);
   console.log('args', args);
   return new Promise((resolve, reject) => {
-    // using @actions/exec:
     options.listeners = {
       stdout: (data) => {
         const stdout = data.toString();
-        console.log('stdout');
-        resolve(stdout.replace(/\n/g, ''));
+        resolve(stdout);
       },
       stderr: (data) => {
         const stderr = data.toString();
-        console.log('stderr');
-        core.setFailed(stderr);
         reject(stderr);
       }
     };
 
     exec.exec(command, args, options)
-      .then(r => console.log('resolved', r))
       .catch(err => {
-        core.setFailed(err);
         reject(err);
       });
-    // using child_process.exec:
-//     childProcessExec(`${command} ${args.join(' ')}`, options, (error, stdout, stderr) => {
-//       if (error) {
-//         console.log(`exec error: ${error}`);
-// //         core.setFailed(error);
-//         resolve(error);
-//         return;
-//       }
-//       console.log(`stdout: ${stdout}`);
-//       console.log(`stderr: ${stderr}`);
-//       resolve(stdout);
-//     });
   });
 }
 
-async function gitMergeCheck(branch) {
+async function gitMergeConflicts(branch) {
   const mergeBase = await execCommand('git', ['merge-base', 'HEAD', branch]);
-  return execCommand('git', ['merge-tree', mergeBase, 'HEAD', branch]);
-  // lookup for ^+=======$
+  const mergeTree = await execCommand('git', ['merge-tree', mergeBase, 'HEAD', branch]);
+  return mergeTree.match(/^\+=======$/);
 }
-
 
 async function run() {
   try {
-    const branches = await execCommand("git", ["branch", "-aq"]);
-    console.log('branches', branches);
-    const conflicted_branch = await gitMergeCheck("origin/conflicted_branch");
-    console.log(conflicted_branch);
-    const nonconflicted_branch = await gitMergeCheck("origin/nonconflicted_branch");
-    console.log(nonconflicted_branch);
-    
-    const payload = JSON.stringify(github.context.payload, undefined, 2);
+    let branches = await execCommand("git", ["branch", "-aq"]);
+    let errors = '';
+    branches = branches.split('\n').map(branch => branch.replace(/\*/g, '').replace(/\s/g, ''))
+    for(let branch in branches) {
+      const isConflicted = await gitMergeCheck("origin/conflicted_branch");
+      
+      if (isConflicted) {
+        errors += `branch has conflicts with ${branch}\n`;
+        // TODO: report author of branch, code that conflicts
+      }
+    }
+    if (errors) {
+      core.setFailed(errors);
+    }
   } catch (error) {
     console.log(error);
     core.setFailed(error.message);
